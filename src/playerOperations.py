5b9a5c07bed5bc92f5960bfa97af09ba40df3a3d
Jérémy L'Hour <jlhourENSAE@users.noreply.github.com>
2021-04-11 21:20:47 +0200

    podcast and episode classes, first attempt at managing the listening og the podcast

---
+++ src/playerOperations.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Player operations:
+    defines the player operations
+Created on Sun Apr 11 21:11:14 2021
+
+@author: jeremylhour
+"""
+import yaml
+import feedparser
+import time
+from datetime import datetime
+
+import vlc
+
+from podcastClasses import Podcast, Episode
+
+# -------------------------------------- 
+# Functions to manage podcast and time
+# --------------------------------------
+
+def stopListening(episode, player):
+    """
+    stopListening:
+        stop listening to the podcast and update the episode timestamp accordingly
+
+    @param podcast (Episode):
+    @param player (vlc.MediaPlayer):
+    """
+    stopTime = player.get_time()
+    if player.is_playing()==1:
+        player.stop()
+    episode.timestamp = stopTime
+    return None
+
+def resumeListening(episode, player):
+    """
+    resumeListening:
+        resume listening to the episode
+
+    @param podcast (Episode):
+    @param player (vlc.MediaPlayer):
+    """
+    if player.is_playing()==0:
+        player.play()
+    player.set_time(episode.timestamp)
+    return None
+
+def jumpTime(player, jump=30):
+    """
+    jumpTime:
+        jump in episode time
+
+    @param player (vlc.MediaPlayer):
+    @param jump (int): a time jump, in seconds
+    """
+    if player.is_playing()==1:
+        currentTime = player.get_time()
+        player.set_time(currentTime + jump*1000)
+    return None
+
+
+if __name__=='__main__':
+    config_file = 'subscriptions.yml'
+    with open(config_file, 'r') as stream:
+        config = yaml.safe_load(stream)
+        
+    for podcast in config['subscriptions']:
+        print(podcast)
+        url = config['subscriptions'][podcast]
+        
+    podcast = Podcast(url)
+    history = podcast.getLastEpisode()
+    
+    newEpisode = next(history)
+    
+    # Play the podcast
+    player = vlc.MediaPlayer(aa.audioUrl)
+    player.play()
+    player.get_time()
+    player.set_time(15830)
+    
+    stopListening(aa, player)
+    resumeListening(aa, player)
+    
+    jumpTime(player, jump=30)
---
+++ src/podcastClasses.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Define the podcast objects
+
+Created on Sat Apr 10 21:10:06 2021
+
+@author: jeremylhour
+"""
+import yaml
+import feedparser
+import time
+from datetime import datetime
+
+
+# --------------------------
+# Object classes definition 
+# --------------------------       
+
+class Podcast():
+    """
+    Podcast:
+        Podcast object, created from url of the RSS feed
+    """
+    
+    def __init__(self, url):
+        """
+        init the object and directly parse the url
+        
+        @param url (str): url for the RSS feed of the podcast
+        """
+        self.url = url
+        self.feed = feedparser.parse(self.url)
+        self.title = self.feed.feed.get('title')
+    
+    def getLastEpisode(self):
+        """
+        getLastEpisode:
+            A generator to get the last episode, goes from more recent to older
+        """
+        for feedEntry in self.feed.entries:
+            publishedDate = datetime.fromtimestamp(time.mktime(feedEntry.published_parsed))
+            podcastEpisode = Episode(podcastName=self.title,
+                                     title=feedEntry['title'],
+                                     date=publishedDate,
+                                     summary=feedEntry['summary'],
+                                     audioUrl=_extractAudioUrl(feedEntry)
+                                     )
+            yield podcastEpisode
+
+
+class Episode():
+    """
+    Episode:
+        A class for episode object
+    """
+    def __init__(self, podcastName, title, date, summary, audioUrl):
+        """
+        @param podcastName (str): title of the podcast
+        @param title (str): title of the episode
+        @param date (datetime.datetime): date of publication
+        @param summary (str): summary of the podcast
+        @param audiorUrl (str): url to the audio file
+        """
+        self.podcastName = podcastName
+        self.title = title
+        self.date = date
+        self.summary = summary
+        self.audioUrl = audioUrl
+        self.timestamp = 0
+    
+    def updateTimestamp(self, newTimestamp):
+        """
+        updateTimestamp:
+            update the Timestamp when the podcast stops playing
+            
+        @param newTimestamp (int): new starting time
+        """
+        self.timestamp = newTimestamp
+        
+    def resetTimestamp(self):
+        """
+        resetTimestamp:
+            reset the timestamp
+        """
+        self.timestamp = 0
+        
+    def toDict(self):
+        """
+        toDict:
+            passes arg to dict, so it can be saved to database
+        """
+        dico = {'podcastName': self.podcastName,
+                'title': self.title,
+                'date': self.date.strftime("%d/%m/%Y, %H:%M:%S"),
+                'audioUrl': self.audioUrl,
+                'timestamp': self.timestamp
+                }
+        return dico
+
+
+# -------
+# Utils
+# -------
+
+def _extractAudioUrl(feedEntry):
+    """
+    extractAudioUrl:
+        extract the url of the audio episode from the feed entry
+        
+    @param feedEntry (dict): a dict that might have an url linking to an audio
+    """
+    for link in feedEntry['links']:
+        if 'audio' in link['type']:
+            audioLink = link['href']
+    return audioLink
+        
+
+
+if __name__=='__main__':
+    config_file = 'subscriptions.yml'
+    with open(config_file, 'r') as stream:
+        config = yaml.safe_load(stream)
+        
+    for podcast in config['subscriptions']:
+        print(podcast)
+        url = config['subscriptions'][podcast]
+        
+    podcast = Podcast(url)
+    history = podcast.getLastEpisode()
+    
+    newEpisode = next(history)
